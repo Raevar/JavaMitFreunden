cf579a44aaf244cff62dba6c00c7420194695896
Maximillian Holzvoig <Maximillian Holzvoig@Android.Speedport_W_921V_1_43_000>
2017-07-18 23:00:22 +0200

    test #ka

--- GeometryDash/src/AudioFilePlayer.java
+++ GeometryDash/src/AudioFilePlayer.java
@@ -1,19 +1,13 @@
 import java.io.File;
 import java.io.FileInputStream;
-
 import javazoom.jl.player.Player;
 
 public class AudioFilePlayer implements Runnable {
-	public AudioFilePlayer() {
-
-	}
 
 	public void run() {
 		while (true) {
 			try {
-				FileInputStream in = new FileInputStream(new File("./Techno.mp3"));
-				Player pl = new Player(in);
-				pl.play();
+				new Player(new FileInputStream(new File("./Techno.mp3"))).play();
 			} catch (Exception e) {
 			}
 		}
