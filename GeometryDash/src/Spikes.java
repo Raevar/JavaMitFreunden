2f238cf9528be30a87ca1a0c4849dc8cc63c52e0
Maximillian Holzvoig <Maximillian Holzvoig@Android.Speedport_W_921V_1_43_000>
2017-08-02 23:19:54 +0200

    Improved first mesh
    Spikes have now a better hit detection

--- GeometryDash/src/Spikes.java
+++ GeometryDash/src/Spikes.java
@@ -3,34 +3,37 @@ import java.awt.Graphics;
 
 public class Spikes extends Obstacles {
 
-	public Spikes(int x, int y,int width, int height, Color color) {
-		super(x, y,width,height, color);
+	public Spikes(int x, int y, int width, int height, Color color) {
+		super(x, y - height, width, height * 2, color);
 	}
 
 	public void paint(Graphics g) {
 		g.setColor(color);
-		g.fillPolygon(new int[] { 0, getWidth() / 2, getWidth() }, new int[] { getHeight(), 0, getHeight() }, 3);
+		g.fillPolygon(new int[] { 0, getWidth() / 2, getWidth() },
+				new int[] { getHeight(), getHeight() / 2, getHeight() }, 3);
 	}
 
-//	public boolean onTop(int x, int y, int w, int h) {
-//		return getY() == y + h && getX() <= x + w && getX() + getWidth() > x;
-//	}
-//
-//	public boolean isSide(int x, int y, int w, int h) {
-//		if (x + w < getX() || x > getX() + getWidth() || y + h <= getY() || y >= getY() + getHeight())
-//			return false;
-//		if (x > getX() + getWidth() / 2)//rechts
-//		{
-//			
-//		} else // links
-//		{
-//			int d = (x - getX())* 2;
-//			if(y+ h + d < getY())
-//				return false;
-//			return true;
-//		}
-//		if (x + w < getX() || x > getX() + getWidth() || y + h <= getY() || y >= getY() + getHeight())
-//			return false;
-//		return true;
-//	}
+	public boolean onTop(int x, int y, int w, int h) {
+		return isHit(x, y, w, h);
+	}
+
+	public boolean isSide(int x, int y, int w, int h) {
+		return isHit(x, y, w, h);
+	}
+
+	public boolean isHit(int x, int y, int w, int h) {
+		int relXLeft = x - getX();
+		int relXRight = relXLeft + w;
+		int relY = y + h - getY();
+		if (relXRight < 0 || relXLeft > getWidth() || relY < getHeight() / 2 || relY - h > getHeight())
+			return false;
+		if (relXLeft > 50) {
+			if (relXLeft < relY)
+				return false;
+			return true;
+		}
+		if (relXRight < getHeight() - relY)
+			return false;
+		return true;
+	}
 }
